682822a7011df0dd39ed7e7e8b152075e35c351e
Yasser <yasser.bel93@gmail.com>
2023-01-02 00:30:34 +0100

    Integrate the routes in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Employee from "./pages/Employee";
+import CreateEmployee from "./pages/CreateEmployee";
+import Error from "./pages/Error";
+
 function App() {
   return (
-    <>Dashboard</>
+    <>
+      <BrowserRouter>
+        {/* ROUTES */}
+        <Routes>
+          <Route path="/" end element={<Dashboard />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/create-employee" element={<CreateEmployee />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="/*" element={<Error />}/>
+        </Routes>
+
+        {/* SIDE BAR */}
+
+        {/* CONTENT */}
+      </BrowserRouter>
+    </>
   );
 }
 
