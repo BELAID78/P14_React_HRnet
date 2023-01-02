7b11af8330f6d238208c42b9a08a3ba12ac1a00d
Yasser <yasser.bel93@gmail.com>
2023-01-02 17:28:30 +0100

    Create departments mocked data

---
+++ src/__mock__/departments.js
@@ -0,0 +1,24 @@
+const departments = [
+    {
+        id: 's0001',
+        value: 'Sales'
+    },
+    {
+        id: 's0002',
+        value: 'Marketing'
+    },
+    {
+        id: 's0003',
+        value: 'Engineering'
+    },
+    {
+        id: 's0004',
+        value: 'Human Resources'
+    },
+    {
+        id: 's0005',
+        value: 'Legal'
+    }
+];
+
+export default departments;
