af580177f98e2014dff45a037bf24a4c27929cab
Yasser <yasser.bel93@gmail.com>
2023-01-02 22:23:24 +0100

    add animation to TextInput

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 import './textInput.css';
 
 function TextInput({label, htmlFor, type, classname}) {
+    const [active, setActive] = useState(false);
+    const inputRef = useRef();
+
+    const handleActive = () => {
+        setActive(true)
+    }
+
+    const handleBlur = () => {
+        if(inputRef.current.value === '') {
+            setActive(false)
+        }
+    }
 
     return (
         <div className={'text-input ' + classname}>
-            <label htmlFor={htmlFor}>{label}</label>
-            <input id={htmlFor} type={type} />
+            <label className={active ? 'active' : ''} htmlFor={htmlFor}>{label}</label>
+            <input ref={inputRef} id={htmlFor} type={type}  onFocus={handleActive} onBlur={handleBlur} />
         </div>
     )
 }
