83731f990c6892017d14eb8efcb1c2c8485a5114
Yasser <yasser.bel93@gmail.com>
2023-01-02 17:47:36 +0100

    complete Employee informations content

--- src/components/CreateEmployeeForm/CreateEmployeeForm.js
+++ src/components/CreateEmployeeForm/CreateEmployeeForm.js
@@ -2,12 +2,43 @@ import React from 'react'
 
 import './createEmployeeForm.css'
 
+import departments from '../../__mock__/departments'
+
 function CreateEmployeeForm() {
     return (
         <div className='create-form'>
             <form>
                 <div className='employee-informations'>
                     <h3>Employee informations:</h3>
+
+                    <div className='text-input'>
+                        <label htmlFor='first-name'>First Name</label>
+                        <input id='first-name' type='text' />
+                    </div>
+
+                    <div className='text-input'>
+                        <label htmlFor='last-name'>Last Name</label>
+                        <input id='last-name' type='text' />
+                    </div>
+
+                    <div className='date-input'>
+                        <label htmlFor='date-of-birth'>Date of Birth</label>
+                        <input id='date-of-birth' type='date' />
+                    </div>
+
+                    <div className='date-input'>
+                        <label htmlFor='start-date'>Start Date</label>
+                        <input id='start-date' type='date' />
+                    </div>
+
+                    <div className='select-input'>
+                        <label htmlFor='department'>Department</label>
+                        <select id='department'>
+                            {
+                                departments.map(item => <option key={item.id}>{item.value}</option>)
+                            }
+                        </select>
+                    </div>
                 </div>
                 <div className='employee-address'>
                     <h3>Employee address:</h3>
