d7f7a8b0a7b0e2f9454308e6c05608b8e960c0c2
Yasser <yasser.bel93@gmail.com>
2023-01-03 04:51:03 +0100

    Add Header and Breadcrumb to employee page

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import Header from '../components/Header/Header';
+import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
 
 function Employee() {
     return (
-        <div>Employee</div>
+        <div className='content'>
+            <Header>
+                <h2>EMPLOYEE</h2>
+            </Header>
+
+            <Breadcrumb>
+                <NavLink to='/employee'>Employee</NavLink>
+            </Breadcrumb>
+        </div>
     )
 }
 
