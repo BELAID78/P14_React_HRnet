612762f935254a65e4a00f8baa91fe0e3663ba68
Yasser <yasser.bel93@gmail.com>
2023-01-03 19:10:34 +0100

    update modal to handle close open

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import Button from '../Button/Button'
 
 import './modal.css'
 
-function Modal({ children, title, modalSize = 'sm', modalHandler = { isOpen: true, setIsOpen: () => {} } }) {
-    const [isOpen, setIsOpen] = useState(modalHandler.isOpen)
+function Modal({ children, title, modalSize = 'sm', modalHandler = { isOpen: false, setIsOpen: () => {} } }) {
+    const clickCloseHandle = () => {
+        modalHandler.setIsOpen(false)
+    }
 
     return (
-        <div className={isOpen ? 'modal fade-in' : 'modal'}>
+        <div className={modalHandler.isOpen ? 'modal fade-in' : 'modal'}>
 
             <div className={`modal-content ${modalSize}`}>
 
@@ -16,7 +18,7 @@ function Modal({ children, title, modalSize = 'sm', modalHandler = { isOpen: tru
 
                     <h4 className='modal-title'>{title}</h4>
 
-                    <span className='modal-close-btn'>X</span>
+                    <span className='modal-close-btn' onClick={clickCloseHandle} >X</span>
                 </div>
 
                 <div className='modal-body'>
@@ -26,7 +28,10 @@ function Modal({ children, title, modalSize = 'sm', modalHandler = { isOpen: tru
                 </div>
 
                 <div className='modal-footer text-right'>
-                    <Button label='Close' classname='btn btn-pink btn-rounded' />
+                    <Button 
+                        label='Close' 
+                        classname='btn btn-pink btn-rounded' 
+                        onclick={clickCloseHandle} />
                 </div>
             </div>
         </div>
