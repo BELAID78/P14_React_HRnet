a2dda8c0ea4807ef9af295b9d41816a0e08d5e8e
Yasser <yasser.bel93@gmail.com>
2023-01-03 08:24:10 +0100

    Add datepicker in dateinput component and styles

--- src/components/DateInput/DateInput.js
+++ src/components/DateInput/DateInput.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
 import './dataInput.css'
 
 function DateInput({label, htmlFor}) {
+    const [date, setDate] = useState(new Date());
+
     return (
         <div className='date-input'>
             <label htmlFor={htmlFor}>{label}</label>
-            <input id={htmlFor} type='date' />
+            <DatePicker
+                id={htmlFor} 
+                selected={date}
+                onChange={(date) => setDate(date)} />
         </div>
-    )
+    );
 }
 
 export default DateInput
