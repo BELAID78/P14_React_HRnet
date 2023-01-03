e8e65f3da7aa96348de4c0a3b8706d70ce66a53a
Yasser <yasser.bel93@gmail.com>
2023-01-03 04:40:49 +0100

    Add Breadcrumb in the dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import Header from '../components/Header/Header';
+import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
 
 function Dashboard() {
     return (
@@ -8,6 +10,10 @@ function Dashboard() {
             <Header>
                 <h2>DASHBOARD</h2>
             </Header>
+
+            <Breadcrumb>
+                <NavLink to='/'>Dashboard</NavLink>
+            </Breadcrumb>
         </div>
     )
 }
