e92bfe9c8a63fcd2e6ecf8900b3224f04e45a277
Yasser <yasser.bel93@gmail.com>
2023-01-03 16:04:43 +0100

    integrate the select change

--- src/components/SelectInput/SelectInput.js
+++ src/components/SelectInput/SelectInput.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 import './selectInput.css'
 
-function SelectInput({label, htmlFor, data}) {
+function SelectInput({label, htmlFor, data, value = '', onchange = () => {}}) {
+    const handleChange = (e) => {
+        onchange(e.target.value)
+    }
+
     return (
         <div className='select-input'>
             <label htmlFor={htmlFor}>{label}</label>
-            <select id={htmlFor}>
+            <select value={value} onChange={handleChange} id={htmlFor}>
                 {
-                    data.map(item => <option key={item.id}>{item.value}</option>)
+                    data.map(item => <option key={item.id} value={item.id} >{item.value}</option>)
                 }
             </select>
         </div>
