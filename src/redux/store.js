30247b5700f1b08cc4d747ad1f1334ac2fe38825
Yasser <yasser.bel93@gmail.com>
2023-01-03 08:55:12 +0100

    Create & integrate redux store

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import employeeReducer from './employeeSlice';
+
+export default configureStore({
+	reducer: {
+		employees: employeeReducer
+	},
+});
