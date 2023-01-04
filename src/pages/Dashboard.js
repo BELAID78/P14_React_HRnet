6c79a1f6e721a8135c283623877c00b2036dc0cb
Yasser <yasser.bel93@gmail.com>
2023-01-04 11:05:39 +0100

    integrate Card in dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,6 +4,11 @@ import { NavLink } from 'react-router-dom'
 import Header from '../components/Header/Header';
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
 
+import usersAsSvg from '../images/users';
+import statesAsSvg from '../images/states';
+import departmentsAsSvg from '../images/departments';
+import Card from '../components/Card/Card';
+
 function Dashboard() {
     return (
         <div className='content'>
@@ -14,6 +19,39 @@ function Dashboard() {
             <Breadcrumb>
                 <NavLink to='/'>Dashboard</NavLink>
             </Breadcrumb>
+
+            <div className='dashboard-container'>
+                <div className='total-container'>
+                    <Card 
+                        title='TOTAL USERS' 
+                        svg={usersAsSvg} 
+                        total='100' 
+                        label='Users' 
+                        className='users-card' />
+
+                    <Card 
+                        title='TOTAL STATES' 
+                        svg={statesAsSvg} 
+                        total='58' 
+                        label='States' 
+                        className='states-card' />
+
+                    <Card 
+                        title='TOTAL DEPARTMENTS' 
+                        svg={departmentsAsSvg} 
+                        total='5' 
+                        label='Departments' 
+                        className='departments-card' />
+                </div>
+                <div className='chart-container'>
+                    <div className='users-by-department'>
+
+                    </div>
+                    <div className='users-by-start-date'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
