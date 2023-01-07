b01399321ac4288a3f752b0a36f4bce9393535f8
Yasser <yasser.bel93@gmail.com>
2023-01-07 13:34:09 +0100

    integrate icons in the dashboard

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import dashboard from './../../images/dashboard'
+import addUser from './../../images/add_user'
+import users from './../../images/users'
+
 import './sidebar.css'
 
 function SideBar() {
@@ -14,16 +18,25 @@ function SideBar() {
 
             <ul className='list'>
                 <li className='list-item'>
-                    <NavLink to='/'>Dashboard</NavLink>
+                    <NavLink to='/'>
+                        <i className='dashboard-icon'>{dashboard}</i>
+                        Dashboard
+                    </NavLink>
                 </li>
                 <li className='devider-list-item'>
                     Employees
                 </li>
                 <li className='list-item'>
-                    <NavLink to='employee'>All</NavLink>
+                    <NavLink to='employee'>
+                        <i className='users-icon'>{users}</i>
+                        All
+                    </NavLink>
                 </li>
                 <li className='list-item'>
-                    <NavLink  to='create-employee'>Create</NavLink>
+                    <NavLink to='create-employee'>
+                        <i className='add-user-icon'>{addUser}</i>
+                        Create
+                    </NavLink>
                 </li>
             </ul>
 
