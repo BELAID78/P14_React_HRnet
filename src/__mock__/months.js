b38a635e1686781572e41689447ea82f347cd64e
Yasser <yasser.bel93@gmail.com>
2023-01-07 04:14:34 +0100

    Create months mooked data

---
+++ src/__mock__/months.js
@@ -0,0 +1,52 @@
+const months = [
+    {
+        id: 1,
+        value: 'January'
+    },
+    {
+        id: 2,
+        value: 'February'
+    },
+    {
+        id: 3,
+        value: 'March'
+    },
+    {
+        id: 4,
+        value: 'April'
+    },
+    {
+        id: 5,
+        value: 'May'
+    },
+    {
+        id: 6,
+        value: 'June'
+    },
+    {
+        id: 7,
+        value: 'July'
+    },
+    {
+        id: 8,
+        value: 'August'
+    },
+    {
+        id: 9,
+        value: 'September'
+    },
+    {
+        id: 10,
+        value: 'October'
+    },
+    {
+        id: 11,
+        value: 'November'
+    },
+    {
+        id: 12,
+        value: 'December'
+    },
+]
+
+export default months
