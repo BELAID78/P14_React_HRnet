3d2976e72050492a84c42935ce2ddfdcbeb921aa
Yasser <yasser.bel93@gmail.com>
2023-01-07 23:58:46 +0100

    complete the error page & style

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleSideBar } from '../redux/sideBarSlice'
 
 function Error() {
+    const dispatch = useDispatch()
+
+    dispatch(toggleSideBar(false))
+
     return (
-        <div>Error</div>
+        <div className='error-container'>
+            <h1>ERROR 404</h1>
+            <p>The request page not found</p>
+        </div>
     )
 }
 
